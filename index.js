efbf337d04af2cfc82359b7204189ef0c08abf25
Vlad Haba <vlad02cristi@yahoo.com>
2023-10-14 20:56:10 +0200

    Resize the class name and use add and remove for class changes

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 //classes list and selector
-let buttonClassList = ["btn btn-danger position-absolute top-50 start-50 translate-middle",
-                    "btn btn-warning position-absolute top-50 start-50 translate-middle",
-                    "btn btn-success position-absolute top-50 start-50 translate-middle"];
+let buttonClassList = ["btn-danger", "btn-warning", "btn-success"];
 
 let buttonClassSelector = 0;
 
@@ -10,8 +8,9 @@ let buttonElement = document.getElementById("button");
 
 // change the color
 function changeButtonColor() {
+    buttonElement.classList.remove(buttonClassList[buttonClassSelector]);
     buttonClassSelector = ++buttonClassSelector % buttonClassList.length;// next index number from buttonClassList
-    buttonElement.className = buttonClassList[buttonClassSelector];
+    buttonElement.classList.add(buttonClassList[buttonClassSelector]);
 };
 
 // add a continue change to element at 10 seconds
@@ -20,10 +19,10 @@ let repeatIntervalBtColor = window.setInterval(function () {
 },  10000);
 
 // add a change to the element at click and at every 10 seconds
-buttonElement.addEventListener("click", function() {
+buttonElement.onclick = function() {
     window.clearInterval(repeatIntervalBtColor); // clear the last repetitive interval
     changeButtonColor();
     repeatIntervalBtColor = window.setInterval(function () { 
         changeButtonColor();
     },  10000);
-});
+};
